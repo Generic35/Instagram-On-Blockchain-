94095a776bed3c1f47298836383d9f9c44ced355
Eric Lamontagne <ricklamontagne@gmail.com>
2022-06-03 09:57:54 +0200

    feat: Add ability to upload an image

--- src/contracts/Decentragram.sol
+++ src/contracts/Decentragram.sol
@@ -2,4 +2,34 @@ pragma solidity ^0.5.0;
 
 contract Decentragram {
     string public name = "Decentragram";
+    // Store Images
+    uint256 public imageCount = 0;
+    mapping(uint256 => Image) public images;
+
+    struct Image {
+        uint256 id;
+        string hash;
+        string description;
+        uint256 tipAmount;
+        address payable author;
+    }
+
+    event ImageCreated(
+        uint256 id,
+        string hash,
+        string description,
+        uint256 tipAmount,
+        address payable author
+    );
+
+    // Create Images
+    function uploadImage(string memory _imgHash, string memory _description)
+        public
+    {
+        imageCount++;
+        // add image to contract
+        images[1] = Image(imageCount, _imgHash, _description, 0, msg.sender);
+        emit ImageCreated(imageCount, _imgHash, _description, 0, msg.sender);
+    }
+    // Tip Images
 }
--- test/test.js
+++ test/test.js
@@ -1,3 +1,5 @@
+const { assert } = require('chai');
+
 const Decentragram = artifacts.require('./Decentragram.sol');
 
 require('chai').use(require('chai-as-promised')).should();
@@ -21,6 +23,32 @@ contract('Decentragram', ([deployer, author, tipper]) => {
     it('has a name', async () => {
       const name = await decentragram.name();
       assert.equal(name, 'Decentragram');
+    });
+  });
+
+  describe('images', async () => {
+    let result, imageCount;
+    const hash = 'abc123';
+
+    before(async () => {
+      result = await decentragram.uploadImage(hash, 'Image description', {
+        from: author,
+      });
+      imageCount = await decentragram.imageCount();
+    });
+
+    it('creates images', async () => {
+      assert.equal(imageCount, 1);
+      const event = result.logs[0].args;
+      assert.equal(event.id.toNumber(), imageCount.toNumber(), 'id is correct');
+      assert.equal(event.hash, hash, 'Hash is correct');
+      assert.equal(
+        event.description,
+        'Image description',
+        'description is correct'
+      );
+      assert.equal(event.tipAmount, '0', 'tip amount is correct');
+      assert.equal(event.author, author, 'author is correct');
     });
   });
 });
