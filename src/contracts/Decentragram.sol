7716f1af0848812e5b5a9a3fb8a27a4c6ded4ef6
Eric Lamontagne <ricklamontagne@gmail.com>
2022-06-06 11:18:15 +0200

    feat: Add ability to tip the author of the instagram post

--- src/contracts/Decentragram.sol
+++ src/contracts/Decentragram.sol
@@ -22,6 +22,14 @@ contract Decentragram {
         address payable author
     );
 
+    event ImageTipped(
+        uint256 id,
+        string hash,
+        string description,
+        uint256 tipAmount,
+        address payable author
+    );
+
     // Create Images
     function uploadImage(string memory _imgHash, string memory _description)
         public
@@ -35,5 +43,28 @@ contract Decentragram {
         images[1] = Image(imageCount, _imgHash, _description, 0, msg.sender);
         emit ImageCreated(imageCount, _imgHash, _description, 0, msg.sender);
     }
+
     // Tip Images
+    function tipImageOwner(uint256 _id) public payable {
+        // Make sure the id is valid
+        require(_id > 0 && _id <= imageCount);
+
+        Image memory _image = images[_id];
+
+        address payable _author = _image.author;
+
+        address(_author).transfer(msg.value);
+
+        _image.tipAmount = _image.tipAmount + msg.value;
+
+        images[_id] = _image;
+
+        emit ImageTipped(
+            _id,
+            _image.hash,
+            _image.description,
+            _image.tipAmount,
+            _author
+        );
+    }
 }
